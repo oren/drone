d57444320161101c65c1aa36363000340bf0580b
Oren <orengolan@gmail.com>
2014-12-11 00:26:31 -0800

    fix broken test

--- server_test.go
+++ server_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func TestIndex(t *testing.T) {
-    const s, sep, want = "chicken", "ken", 3
+    const s, sep, want = "chicken", "ken", 4
     got := strings.Index(s, sep)
     if got != want {
         t.Errorf("Index(%q,%q) = %v; want %v", s, sep, want, got)
